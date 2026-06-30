c1a499553b36463e904bd94c26184169195692b6
royromerog_github <royromerog@gmail.com>
2026-06-30 11:29:59 -0600

    query top 10 paises que generan más ingresos

--- sql/01_exploracion.sql
+++ sql/01_exploracion.sql
@@ -1,3 +1,12 @@
+
+/*
+---------------------------------------------------------
+Ejercicio 1
+
+Pregunta de negocio:
+¿Cuales son los 5 países que mas han gastado?
+---------------------------------------------------------
+*/
 SELECT c.Country,
 	count(c.CustomerId) as gasto_total
 FROM Customer AS c
@@ -7,13 +16,27 @@ GROUP BY c.Country
 ORDER by gasto_total DESC
 LIMIT 5;
 
+/*
+---------------------------------------------------------
+Ejercicio 2
 
+Pregunta de negocio:
+¿Cuántos clientes existen por país?
+---------------------------------------------------------
+*/
 SELECT Country,
-	count(CustomerId) as num_clientes
+	count(*) as num_clientes
 FROM Customer AS c 
 GROUP BY Country; 
 
+/*
+---------------------------------------------------------
+Ejercicio 3
 
+Pregunta de negocio:
+¿Cual es el promedio de gasto por País?
+---------------------------------------------------------
+*/
 SELECT c.Country,
 	avg(i.Total) AS avg_gast
 FROM Customer AS c
